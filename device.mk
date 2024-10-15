768870691a6ad08a6c5fd839c8fc3ed1789f0933
ArianK16a <ArianK16a@users.noreply.github.com>
2024-10-15 03:53:41 +0000

    garnet: Build libcodec2_soft_common.vendor
    Signed-off-by: Hikari-Soni <247soni01@gmail.com>

--- device.mk
+++ device.mk
@@ -324,6 +324,7 @@ PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.2.vendor \
     libOmxCore \
     libcodec2_hidl@1.0.vendor \
+    libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
     libstagefrighthw \
     libstagefright_softomx_plugin.vendor
